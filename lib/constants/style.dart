ff8050f10e8c3b98842512f442f96850c5162cea
fikriymnn <fikriyomann@student.upi.edu>
2023-06-07 13:03:53 +0700

    update warna,slidebar

--- lib/constants/style.dart
+++ lib/constants/style.dart
@@ -6,3 +6,4 @@ Color lightGrey = Color(0xFFA4A6B3);
 Color dark = Color(0xFF363740);
 Color active = Color(0xFFED1D25);
 Color green = Color(0xffB1CC85);
+Color blue = Color(0xff0550A2);
--- lib/screens/dashboard_screens/dashboard_screen.dart
+++ lib/screens/dashboard_screens/dashboard_screen.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:indlkt_proj/widgets/slide_bar.dart';
 
 class DashboardScreen extends StatelessWidget {
   const DashboardScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: SlideBar(),
+    );
   }
 }
