dce131d1c52410ae3726f2881bf20319fd9ee3a9
acepW <acepwahyu001@gmail.com>
2023-06-07 11:11:57 +0700

    update branch tio

--- lib/screens/login_screen/login_screen.dart
+++ lib/screens/login_screen/login_screen.dart
@@ -5,6 +5,41 @@ class LoginScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Row(
+          children: [
+            Column(
+              children: [
+                Container(
+                  color: Colors.white,
+                  height: 200,
+                  width: 600,
+                  child: Image(
+                    image: AssetImage("assets/images/logo.png"),
+                    fit: BoxFit.contain,
+                  ),
+                ),
+                Container(
+                  child: Text("Welcome To Inventory Management"),
+                )
+              ],
+            ),
+            Column(
+              children: [
+                Container(
+                  child: Image(
+                    image: AssetImage("assets/images/image_32.png"),
+                    fit: BoxFit.contain,
+                  ),
+                  width: 927,
+                  height: 600,
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
